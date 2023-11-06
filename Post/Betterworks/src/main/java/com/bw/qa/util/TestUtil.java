61b28d9797da9de1d47581cbe49d64213c751d87
Aishwaryavenkat10 <aishwaryavenkat62@gmail.com>
2023-11-06 15:53:02 +0530

    Wait time in test util class is changes

--- Post/Betterworks/src/main/java/com/bw/qa/util/TestUtil.java
+++ Post/Betterworks/src/main/java/com/bw/qa/util/TestUtil.java
@@ -2,6 +2,6 @@ package com.bw.qa.util;
 
 public class TestUtil {
 
-    public   static long PAGE_LOAD_TIMEOUT = 10;
-    public   static long IMPLICIT_WAIT = 10;
+    public   static long PAGE_LOAD_TIMEOUT = 20;
+    public   static long IMPLICIT_WAIT = 20;
 }
